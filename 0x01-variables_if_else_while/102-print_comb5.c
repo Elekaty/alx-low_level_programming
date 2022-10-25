0d37974a34f11f63ebe36dad914d5ccd8da7bdb8
Elekaty <kate.sele1@gmail.com>
2022-10-25 03:55:16 -0700

    tk909

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,25 @@
 
 int main(void)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j <= 99; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
-			if (i != 98 || j != 99)
+			if (i == 98 && j == 99)
 			{
-				putchar(',');
-				putchar(' ');
+				break;
 			}
+
+			putchar(',');
+			putchar (' ');
 		}
 	}
 	putchar('\n');
