0856e8d56f5a2509efe1e236e17ed59581594d2d
Elekaty <kate.sele1@gmail.com>
2022-10-27 08:18:04 -0700

    th006

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,39 +2,41 @@
 
 /**
  * cap_string - capitalizes all words
- * @sr: the string to be capitalized
+ * @str: the string to be capitalized
  *
  * Return: A pointer to the changed string
  */
 
-char *cap_string(char *sr)
+char *cap_string(char *str)
 {
 	int num = 0;
 
-	while (sr[num])
+	while (str[num])
 	{
-		while (!sr([num] >= 'a' && sr[num] <= 'z'))
-			num++;
-		if (sr[num - 1 == ' ' ||
-				sr[num - 1 == ' ' ||
-				sr[num - 1 == '\t' ||
-				sr[num - 1 == '\n' ||
-				sr[num - 1 == ',' ||
-				sr[num - 1 == ';' ||
-				sr[num - 1 == '.' ||
-				sr[num - 1 == '!' ||
-				sr[num - 1 == '?' ||
-				sr[num - 1 == '"' ||
-				sr[num - 1 == '(' ||
-				sr[num - 1 == ')' ||
-				sr[num - 1 == ' ' ||
-				sr[num - 1 == '{' ||
-				sr[num - 1 == '}' ||
+		while (!str([num] >= 'a' && sr[num] <= 'z'))
+
+			num++
+
+		if (str[num - 1] == ' ' ||
+				str[num - 1] == ' ' ||
+				str[num - 1] == '\t' ||
+				str[num - 1] == '\n' ||
+				str[num - 1] == ',' ||
+				str[num - 1] == ';' ||
+				str[num - 1] == '.' ||
+				str[num - 1] == '!' ||
+				str[num - 1] == '?' ||
+				str[num - 1] == '"' ||
+				str[num - 1] == '(' ||
+				str[num - 1] == ')' ||
+				str[num - 1] == ' ' ||
+				str[num - 1] == '{' ||
+				str[num - 1] == '}' ||
 				num == 0)
-			sr[num] -= 32;
+			str[num] -= 32;
 		num++;
 	}
-	return (sr);
+	return (str);
 }
 
 
