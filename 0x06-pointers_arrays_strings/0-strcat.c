5e2b599466a8507480644b2f7e4489649ce0c5c7
Elekaty <kate.sele1@gmail.com>
2022-10-31 06:08:27 -0700

    tasko11

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two string pointed to by @src,
- * null bite, to the end of string pointed to by @dest
- * @dest: A pointer to the string to be concatented upon
- * @src: the source string to be appended to @dest
+ * _strcat - concatenates two string pointed
+ * @dest: copy to
+ * @src: copy from
  *
- * Return: pointer to destination string @dest
+ * Return: pointer dest
  */
 char *_strcat(char *dest, char *src)
 {
