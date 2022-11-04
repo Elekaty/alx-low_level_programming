ea0d6b265b7aec62551bed94d7584a5d2768007e
Elekaty <kate.sele1@gmail.com>
2022-11-04 10:13:27 -0700

    tash1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,10 @@
  * main - Entry point
  * @argc: argument
  * @argv: array pointing to argument
- * Return: '0'
+ * Return: 0
  */
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 	return (0);
 }
